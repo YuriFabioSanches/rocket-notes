79191e883ce99257f513687e626efe8843bd7cbe
Yuri Fabio Sanches <yuri.fsanches@hotmail.com>
2024-03-16 17:09:00 -0300

    login screen finished

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,34 @@
-import { Container } from "./styles"
+import { FiMail, FiLock } from "react-icons/fi"
+
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
+
+import { Container, Form, Title, LoginTitle, InputsWrapper, BackgroundImg } from "./styles"
 
 export function SignIn() {
   return(
-    <Container></Container>
+    <Container>
+      <Form>
+        <Title>
+          <h1>Rocket Notes</h1>
+          <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+        </Title>
+
+        <LoginTitle>
+          <h2>Faça seu login</h2>
+        </LoginTitle>
+
+        <InputsWrapper>
+          <Input placeholder="E-mail" type="email" icon={FiMail} />
+          <Input placeholder="Password" type="password" icon={FiLock} />
+        </InputsWrapper>
+
+        <Button title="Entrar" />
+
+        <a href="#">Criar conta</a>
+      </Form>
+      
+      <BackgroundImg />
+    </Container>
   );
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,7 +1,61 @@
 import styled from "styled-components";
+import backgroundImg from '../../assets/background.png'
 
 export const Container = styled.div`
   height: 100vh;
   display: flex;
   align-items: stretch;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 0 13.6rem;
+  
+  a {
+    margin-top: 12.4rem;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+`;
+
+export const Title = styled.div`
+  h1 {
+    font-weight: 700;
+    font-size: 4.8rem;
+    line-height: 6.3rem;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+
+  p {
+    font-size: 1.4rem;
+    line-height: 1.8rem;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+  }
+`;
+
+export const LoginTitle = styled.div`
+  padding: 4.8rem 0;
+
+h2 {
+  font-weight: 500;
+  font-size: 2.4rem;
+  line-height: 3.2rem;
+  color: ${({ theme }) => theme.COLORS.WHITE};
+}
+`;
+export const InputsWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: .8rem;
+  margin-bottom: .8rem;
+`;
+
+export const BackgroundImg = styled.div`
+  flex: 1;
+  background: url(${backgroundImg}) no-repeat center center;
+  background-size: cover;
 `;
