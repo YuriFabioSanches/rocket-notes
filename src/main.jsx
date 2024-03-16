bf0b793065c00173fea3c2e8cced3a45dbd87af1
Yuri Fabio Sanches <yuri.fsanches@hotmail.com>
2024-03-16 15:55:10 -0300

    SignIn page structure created

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import GlobalStyles from './styles/globalStyles'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 
-import { Home } from './pages/Home'
+import { SignIn } from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
