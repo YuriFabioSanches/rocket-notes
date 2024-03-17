801cbc7e95a4f19c24d691882fb392b9f9087833
Yuri Fabio Sanches <yuri.fsanches@hotmail.com>
2024-03-17 14:14:34 -0300

    sign up page finished

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,35 @@
+import { FiMail, FiLock, FiUser } from "react-icons/fi"
+
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
+
+import { Container, Form, Title, LoginTitle, InputsWrapper, BackgroundImg } from "./styles"
+
+export function SignUp() {
+  return(
+    <Container>
+      <BackgroundImg />
+
+      <Form>
+        <Title>
+          <h1>Rocket Notes</h1>
+          <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+        </Title>
+
+        <LoginTitle>
+          <h2>Crie sua conta</h2>
+        </LoginTitle>
+
+        <InputsWrapper>
+          <Input placeholder="Nome" type="text" icon={FiUser} />
+          <Input placeholder="E-mail" type="email" icon={FiMail} />
+          <Input placeholder="Password" type="password" icon={FiLock} />
+        </InputsWrapper>
+
+        <Button title="Cadastrar" />
+
+        <a href="#">Voltar para o login</a>
+      </Form>      
+    </Container>
+  );
+}
---
+++ src/pages/SignUp/styles.js
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import backgroundImg from '../../assets/background.png'
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 0 13.6rem;
+  
+  a {
+    margin-top: 12.4rem;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+
+  animation: appear .5s ease-out;
+
+  @keyframes appear {
+    to{
+      transform: scale(1);
+    }
+    from{
+      transform: scale(0);
+    }
+  }
+`;
+
+export const Title = styled.div`
+  h1 {
+    font-weight: 700;
+    font-size: 4.8rem;
+    line-height: 6.3rem;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+
+  p {
+    font-size: 1.4rem;
+    line-height: 1.8rem;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+  }
+`;
+
+export const LoginTitle = styled.div`
+  padding: 4.8rem 0;
+
+h2 {
+  font-weight: 500;
+  font-size: 2.4rem;
+  line-height: 3.2rem;
+  color: ${({ theme }) => theme.COLORS.WHITE};
+}
+`;
+export const InputsWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: .8rem;
+  margin-bottom: .8rem;
+`;
+
+export const BackgroundImg = styled.div`
+  flex: 1;
+  background: url(${backgroundImg}) no-repeat center center;
+  background-size: cover;
+`;
