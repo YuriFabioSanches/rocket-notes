9186a3c5fb50a6e2443cbf3ea590d93c5125775a
Yuri Fabio Sanches <yuri.fsanches@hotmail.com>
2024-03-18 16:32:03 -0300

    routes folder and files created

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import GlobalStyles from './styles/globalStyles'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 
-import { NewNote } from './pages/NewNote'
+import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <NewNote />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
